329065e47891334908d7ddaf856693ebcfd4171f
Alys <driftksa2014@gmail.com>
2024-05-13 23:22:34 +0300

    adding src

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ document.addEventListener("DOMContentLoaded", async function () {
     const selectElement2 = document.getElementById("typeodDeviceis2");
     const selectElement4 = document.getElementById("typeodDeviceis4");
 
+
+    // const detailElement = document.getElementById("typeodDeviceis3");
     
+    ///Buttons 1 and 3
     try {
         const response = await fetch('/compare/branddata');
         if (!response.ok) throw new Error('Network response was not ok');
@@ -19,7 +22,7 @@ document.addEventListener("DOMContentLoaded", async function () {
         selectElement1.addEventListener('change', async () => {
             try {
                 const selectedId = selectElement1.value;
-                console.log(selectedId);
+                // console.log(selectedId);
                 const deviceResponse = await fetch('/compare/'+ selectedId);
                 if (!deviceResponse.ok) throw new Error('Failed to fetch devices');
                 const devices = await deviceResponse.json();
@@ -37,7 +40,7 @@ document.addEventListener("DOMContentLoaded", async function () {
         console.error('Fetch error:', error);
     }
 
-//////////////////////////////////////
+//////////////////////////////////////buttons 2 and 4
 
 
     try {
@@ -53,7 +56,7 @@ document.addEventListener("DOMContentLoaded", async function () {
         selectElement2.addEventListener('change', async () => {
             try {
                 const selectedId = selectElement2.value;
-                console.log(selectedId);
+                // console.log(selectedId);
                 const deviceResponse = await fetch('/compare/'+selectedId);
                 if (!deviceResponse.ok) throw new Error('Failed to fetch devices');
                 const devices = await deviceResponse.json();
@@ -61,7 +64,7 @@ document.addEventListener("DOMContentLoaded", async function () {
                 devices.forEach(device => {
                     // console.log(device)
                     const option = new Option(device.name,device.id);
-                    console.log(device.id)
+                    // console.log(device.id)
                     selectElement4.appendChild(option);
                 });
             } catch (error) {
@@ -71,10 +74,47 @@ document.addEventListener("DOMContentLoaded", async function () {
     } catch (error) {
         console.error('Fetch error:', error);
     }
+///detailss
+
+selectElement3.addEventListener('change', async () => {
+    try {
+        const selectedId = selectElement3.value;
+        console.log(selectedId);
+        const deviceResponse = await fetch('/compare/details/'+selectedId);
+        if (!deviceResponse.ok) throw new Error('Failed to fetch devices');
+        const device = await deviceResponse.json();
+
+        const imageElement = document.getElementById('pic1')
+        console.log(device);
+        const newImageSrc = device.img;  // Update with the correct path
+        imageElement.src = newImageSrc;
+        const paragraphElement = document.getElementById('Comp1');
+        paragraphElement.textContent;
+        // selectElement4.innerHTML = '';
+        // devices.forEach(device => {
+        //     console.log(device)
+
+        // });
+    } catch (error) {
+        console.error('Error fetching devices:', error);
+    }
 }
+)
+
+
+
+
+}
+
 
 // selectElement3.addEventListener('change', async () => {
 
 
+
+
 // }
+//////Details
+
+
+
 );
