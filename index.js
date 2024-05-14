1b00df14e5d7f05cb961cd289660392282abed57
Mohammed-NG <125130993+Mohammed-NG@users.noreply.github.com>
2024-05-15 01:10:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json()); app.use(bodyParser.urlencoded({ extended: true }));
 
 //app.use(express.urlencoded({extended: false}))
 
-app.use(express.static("public"));
+//app.use(express.static("public"));
 
 
 
